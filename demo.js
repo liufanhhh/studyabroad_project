4dc7c1c9d156352e46e6745cb17a3ce69c3f42df
liufan <1043099804@qq.com>
2014-10-26 10:59:53 +0800

    hope successed

---
+++ demo.js
@@ -0,0 +1,62 @@
+﻿
+	var app = angular.module("MyApp", []);
+	app.controller("Trycontroller",
+    	function ($scope)
+    	{
+    		$scope.status=true;
+			$scope.setregister = function()
+			{	
+				$scope.status=!$scope.agree;
+			};
+
+
+			$scope.aa=function(a){
+				return "待办事项";
+
+			}
+
+ 		$scope.myData = new Date();
+			$scope.register=function()
+			{	
+					
+						if($scope.password1 == $scope.password2)
+							{	
+								$scope.list.push
+									({
+						        reguser: $scope.nguser,
+						        regpassword1: $scope.password1
+									});
+							    $scope.nguser = null;
+							    $scope.password1 = null;
+							    $scope.password2 = null;
+							    $scope.agree = null;
+								$scope.status=true;
+							}
+
+						else
+							{
+								alert("password are not same");
+								$scope.password2=null;
+								
+								
+							}
+			};
+			$scope.reset = function(nowindex)
+			{
+				$scope.list.splice(nowindex,1 );
+
+
+			}
+
+
+
+  			$scope.list = 
+  			[	
+
+			];
+
+
+
+
+
+		});
