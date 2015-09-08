babf953f5389f7be50bd487d59681c530ac09a2d
fan.liu <fan.liu@paymentwall.com>
2015-09-08 19:19:25 +0800

    completed

--- test/test.js
+++ test/test.js
@@ -1,30 +1,67 @@
 var Paymentwall = require('paymentwall');
 Paymentwall.configure(
   Paymentwall.Base.API_BRICK,
-  't_1323f420a53c2cc102e931f7f6710d',
-  't_2ffe77340638b142cca2acdc9cd1cf'
+  'ad678b420aa1cecc4a7950a9eef4d765',
+  '566f94c4e0243740faec24cb86ee0705'
+);
+// try{
+// 	var onetimetoken = new Paymentwall.Onetimetoken(
+// 	4000000000000002,          // Card number, digits only
+// 	01,         // Expiration month, 2 digits from 01 to 12
+// 	2017,           // Expiration year, 4 digits
+// 	121                // CVC/CVV, 3-4 digits
+// 	);
+// 	onetimetoken.createOnetimetoken(function(onetimetoken_response){
+// 	// get the onetimetoken in response
+// 	console.log('onetimetoken='+onetimetoken_response.token);
+// 	});
+// }catch(e){
+// 	console.log(e);
+// }
+
+
+
+
+var ccharge = new Paymentwall.Charge(
+1,
+'USD',
+'fan',
+'helloaa',
+123,
+'ddd',
+{'custom[aad]':'aa'}
 );
 
-// var onetimetoken = new Paymentwall.Onetimetoken(
-// 4000000000000002,          // Card number, digits only
-// 01,         // Expiration month, 2 digits from 01 to 12
-// 2017,           // Expiration year, 4 digits
-// 121                // CVC/CVV, 3-4 digits
-// );
-// onetimetoken.createOnetimetoken(function(onetimetoken_data){
-// // get the onetimetoken in response
-// console.log('onetimetoken='+onetimetoken_data.token);
+ccharge.createCharge(function(Charge_response){
+// get the onetimetoken in response
+console.log('response of Charge ='+Charge_response);
+});
+
+// var charge = new Paymentwall.Charge();
+
+// charge.otherOperation(123121,'detail',function(refund_data){
+// 	console.log('refund_data'+refund_data);
 // });
 
 
-var charge = new Paymentwall.Charge(
-0.5,
-'USD',
-'fan liu test',
-'liufanhh@hotmail.com',
-'ashdf',
-'123',
-{'custom': 'aaa'} 
-);
+// var subscription = new Paymentwall.Subscription(
+// 1,
+// 'USD',
+// 'fan',
+// 'hello.com',
+// 123,
+// 123,
+// 3,
+// 'day',
+// {
+// 	'trial[amount]':1,
+// 	'trial[currency]':'USD',
+// 	'trial[period]':3,
+// 	'trial[period_duration]':'day'
+// }
+// );
 
-charge.createCharge();
+// subscription.createSubscription(function(subscription_response){
+// 	console.log(subscription_response);
+// });
+
