def8799094759daaa84682f19911ed6b22bb72a0
fan.liu <fan.liu@paymentwall.com>
2015-09-09 19:18:52 +0800

    change again

--- test/test.js
+++ test/test.js
@@ -1,35 +1,30 @@
 var Paymentwall = require('paymentwall');
 Paymentwall.configure(
-  Paymentwall.Base.API_BRICK,
-  'ad678b420aa1cecc4a7950a9eef4d765',
-  '566f94c4e0243740faec24cb86ee0705'
+  Paymentwall.Base.API_GOODS,
+  't_1323f420a53c2cc102e931f7f6710d',
+  't_2ffe77340638b142cca2acdc9cd1cf'
 );
-// try{
-// 	var onetimetoken = new Paymentwall.Onetimetoken(
-// 	4000000000000002,          // Card number, digits only
-// 	01,         // Expiration month, 2 digits from 01 to 12
-// 	2017,           // Expiration year, 4 digits
-// 	121                // CVC/CVV, 3-4 digits
-// 	);
-// 	onetimetoken.createOnetimetoken(function(onetimetoken_response){
-// 	// get the onetimetoken in response
-// 	console.log('onetimetoken='+onetimetoken_response.token);
-// 	});
-// }catch(e){
-// 	console.log(e);
-// }
 
+var onetimetoken = new Paymentwall.Onetimetoken(
+4000000000000002,          // Card number, digits only
+01,         // Expiration month, 2 digits from 01 to 12
+2015,           // Expiration year, 4 digits
+121            // CVC/CVV, 3-4 digits
+);
+onetimetoken.createOnetimetoken(function(onetimetoken_response){
+// get the onetimetoken in response
+console.log('onetimetoken='+onetimetoken_response.token);
 
-
+var token = onetimetoken_response.token;
 
 var ccharge = new Paymentwall.Charge(
-1,
+0.5,
 'USD',
 'fan',
-'helloaa',
+'liufanhh@hotmail.com',
 123,
-'ddd',
-{'custom[aad]':'aa'}
+token
+// {'custom[aad]':'aa'}
 );
 
 ccharge.createCharge(function(Charge_response){
@@ -37,9 +32,55 @@ ccharge.createCharge(function(Charge_response){
 console.log('response of Charge ='+Charge_response);
 });
 
+var subscription = new Paymentwall.Subscription(
+1,
+'USD',
+'fan',
+'hello.com',
+123,
+token,
+'day',
+3,
+{
+	'trial[amount]':1,
+	'trial[currency]':'USD',
+	'trial[period]':'day',
+	'trial[period_duration]':3
+}
+);
+
+subscription.createSubscription(function(subscription_response){
+	console.log(subscription_response);
+});
+
+});
+
+
+// 4147371209230706,          // Card number, digits only
+// 11,         // Expiration month, 2 digits from 01 to 12
+// 2017,           // Expiration year, 4 digits
+// 678 
+
+
+
+// var ccharge = new Paymentwall.Charge(
+// 0.5,
+// 'USD',
+// 'fan',
+// 'liufanhh@hotmail.com',
+// 123,
+// 'o_ef0b32907c58146462724a4291d757c6',
+// {'custom[aad]':'aa'}
+// );
+
+// // ccharge.createCharge(function(Charge_response){
+// // // get the onetimetoken in response
+// // console.log('response of Charge ='+Charge_response);
+// // });
+
 // var charge = new Paymentwall.Charge();
 
-// charge.otherOperation(123121,'detail',function(refund_data){
+// charge.otherOperation(123121,'refund',function(refund_data){
 // 	console.log('refund_data'+refund_data);
 // });
 
@@ -64,4 +105,18 @@ console.log('response of Charge ='+Charge_response);
 // subscription.createSubscription(function(subscription_response){
 // 	console.log(subscription_response);
 // });
+
+
+// var subscription = new Paymentwall.Subscription();
+// subscription.otherOperation(1231111,'detail',function(refund_data){
+// 	console.log('refund_data'+refund_data);
+// });
+
+
+
+
+
+
+
+
 
