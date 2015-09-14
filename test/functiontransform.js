0e6226ee8be7b1d126142b11e93f809a99d741ab
fan.liu <fan.liu@paymentwall.com>
2015-09-14 19:10:29 +0800

    set some model files

--- test/functiontransform.js
+++ test/functiontransform.js
@@ -1,29 +1,39 @@
-var http = require("http"); 
-function onRequest(request, response) {   
-	console.log("Request received.");   
-	response.writeHead(200, {"Content-Type": "text/plain"});   
-	response.write("Hello World");   response.end(); } 
+LocalFlexibleRequest request = new LocalFlexibleRequest();
+request.setKey(PROJECT_KEY); // Put project key here
+request.setUid(USER_ID); // User ID (email or username)
+request.setWidget(WIDGET_TYPE); // Widget key, obtained from Widget settings in PW site
+// Could be ApiType.VIRTUAL_CURRENCY, ApiType.CART or ApiType.ApiType.DIGITAL_GOODS
+request.setApiType(ApiType.DIGITAL_GOODS);
+request.setAmount(0.01f); // Payment amount
+request.setCurrencycode("USD"); // Currency code
+request.setAgName(ITEM_NAME); // Product name
+request.setAgExternalId(ITEM_ID); // Product id in your system
+request.setSecretKey(SECRET_KEY);
+request.setSignVersion(3);
+request.setAgType("fixed");
 
-http.createServer(onRequest).listen(8888); 
-console.log("Server has started.");
-<pre escaped="true" lang="javascript" line="1">//person.js
+Intent intent = new Intent(getApplicationContext(), PwLocalActivity.class);
+intent.putExtra(Key.PAYMENT_TYPE, PaymentMethod.PW_LOCAL_FLEXIBLE);
+intent.putExtra(Key.REQUEST_MESSAGE, request);
+startActivityForResult(intent, PwLocalActivity.REQUEST_CODE);
 
-var num = 0;
-exports.Person = function(pName){
- var pri = {
-      pName : " ",
-      pAge : 0
-   }
-  var pub = {
-       setName : function(pName){
-          	pri.pName = pName;
-        },
-        getName : function(){
-            return pri.pName;
-        }
-    }
- //construct code
-    pri.pName = pName;
- 
-    return pub;
- }</pre>
+public static void requestPay(String USER_ID,String WIDGET_TYPE,float amount,String currency,String ITEM_NAME,String ITEM_ID)
+ {
+  LocalFlexibleRequest request = new LocalFlexibleRequest();
+  request.setKey(PROJECT_KEY); // Put project key here
+  request.setUid(USER_ID); // User ID (email or username)
+  request.setWidget(WIDGET_TYPE); // Widget key, obtained from Widget settings in PW site
+  // Could be ApiType.VIRTUAL_CURRENCY, ApiType.CART or ApiType.ApiType.DIGITAL_GOODS
+  request.setApiType(ApiType.DIGITAL_GOODS);
+  request.setAmount(amount); // Payment amount
+  request.setCurrencycode(currency); // Currency code
+  request.setAgName(ITEM_NAME); // Product name
+  request.setAgExternalId(ITEM_ID); // Product id in your system
+  request.setSecretKey(SECRET_KEY);
+  request.setSignVersion(2);
+  request.setAgType("fixed");
+  Intent intent = new Intent(mContext, PwLocalActivity.class);
+  intent.putExtra(Key.PAYMENT_TYPE, PaymentMethod.PW_LOCAL_FLEXIBLE);
+  intent.putExtra(Key.REQUEST_MESSAGE, (Parcelable)request);
+  ((Activity)mContext).startActivityForResult(intent, PwLocalActivity.REQUEST_CODE);
+ }
