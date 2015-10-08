d7bb5ef9f155da168c1e3ee1756c5a09f4f06f84
1043099804 <1043099804@qq.com>
2015-10-08 15:43:34 +0800

    Update functiontransform.js

--- test/functiontransform.js
+++ test/functiontransform.js
@@ -1,29 +1,29 @@
-var http = require("http"); 
-function onRequest(request, response) {   
-	console.log("Request received.");   
-	response.writeHead(200, {"Content-Type": "text/plain"});   
-	response.write("Hello World");   response.end(); } 
 
-http.createServer(onRequest).listen(8888); 
-console.log("Server has started.");
-<pre escaped="true" lang="javascript" line="1">//person.js
+var Paymentwall = require('paymentwall');
+exports.pwpingback = function(req,res){
+// judge if the pingback is sent by Paymentwall
+Paymentwall.configure(
+  Paymentwall.Base.API_GOODS,
+  'YOUR_APPLICATION_KEY',
+  'YOUR_SECRET_KEY'
+);
+var pingback = new Paymentwall.Pingback(queryData, ipAddress);
+if (pingback.validate()) {
+var productId = pingback.getProduct().getId();
+if (pingback.isDeliverable()) {
+// deliver the product
+} else if (pingback.isCancelable()) {
+// withdraw the product
+} 
+console.log('OK'); // Paymentwall expects the string OK in response, otherwise the pingback will be resent
+} else {
+console.log(pingback.getErrorSummary());
+}
 
-var num = 0;
-exports.Person = function(pName){
- var pri = {
-      pName : " ",
-      pAge : 0
-   }
-  var pub = {
-       setName : function(pName){
-          	pri.pName = pName;
-        },
-        getName : function(){
-            return pri.pName;
-        }
-    }
- //construct code
-    pri.pName = pName;
- 
-    return pub;
- }</pre>
+//handle the parameters in req
+var uid = req.query.uid;
+var goodsid = req.query.goodsid;
+
+//handle the response to Paymentwall
+res.send("OK");
+}
